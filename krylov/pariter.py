686c4306ec545844b431dcc0319d6039927b0bca
Steven A Gomez <gomezs@mit.edu>
2013-03-27 17:37:10 -0400

    Cleaned up unecessary arguments to krylov solvers

--- krylov/pariter.py
+++ krylov/pariter.py
@@ -4,7 +4,7 @@ from numpy import *
 from scipy.sparse import linalg as splinalg
 
 
-def conjGrad( A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None, skip=1, M=None ):
+def conjGrad( A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None, M=None ):
     """Implementation of conjugate gradient, mimics api of scipy.sparse.linalg.cg with addition of user supplied inner product.
     --- Also uses Polak-Ribiere formula for beta to improve performance for non fixed preconditioners"""
     n = len(b)
@@ -40,7 +40,7 @@ def conjGrad( A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None, ski
             err = (0, k+1)
             break
 
-        if callback is not None and (k+1)%skip==0:
+        if callback is not None:
             callback(x, r)
 
         z = M*r
@@ -55,14 +55,10 @@ def conjGrad( A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None, ski
 
     return x, err
 
-def minRes(  A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None, skip=1, M=None  ):
+def minRes(  A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None ):
     n = len(b)
 
     sqrt = math.sqrt # local lookup
-
-    if M is None:
-        M = splinalg.LinearOperator( (n,n), matvec=lambda x: x, dtype=float )
-
 
     if maxiter is None:
         maxiter = n*10
@@ -112,61 +108,8 @@ def minRes(  A, b, x0=None, tol=1e-5, maxiter=None, dot=dot, callback=None, skip
         if abs(eta) < tol or abs(eta)/r0 < tol:
             return x, (0, k+1)
 
-        if callback is not None and (k+1)%skip==0:
+        if callback is not None:
             callback(x, b-A*x )
 
     else:
         return x, (1, maxiter)
-
-
-
-def _main():
-    k, nu = 16, 0.001
-    dt = pi / 16.
-    M = 4*k*k*(k+1) * 2 * 3
-    Nt = int(sys.argv[1]) if len(sys.argv) > 1 else 32
-
-    shp = (Nt, M)
-    ftraj = os.path.join( ROOT, 'traj_2048.dat')
-
-    ns = IF3DParallel(k, nu, dt, ftraj, shp, top=True)
-    rhs = ns.perturbation.copy()
-
-    tol = 1e-5
-    levels = [ (False,True), (False,True), (False,True), (True,False) ]
-
-    vcycle = VCycleKrylov(ns, levels, skip = 20, pre_iters = 20, 
-                                      post_iters = 40, tol = tol, method = minRes)
-
-    call = SaveLogCallback( ns, rhs, 
-                            fname = os.path.join(ROOT, 'log.npy'),
-                            outname = os.path.join(ROOT, 'restart_out.dat') )
-
-    if MASTER:
-        print 'init res: {nr:.6e}, rel_res: {rr:.3e}'.format(nr=call.nb, rr=1.0)
-
-    w, err = conjGrad( ns, rhs, dot=ns.dot, callback=call, M=vcycle, tol=tol, maxiter=150)
-    
-    call(w)
-
-    #w_full = ns.comm.collect(w)
-    
-    if MASTER:
-        print('Got Results')
-        print 'CG error =', err
-
-        #save( os.path.join(ROOT, 'MG_results.npy'), w_full)
-
-
-
-if __name__ == '__main__':
-    from numpy import sqrt
-    from scipy.linalg import norm
-    from scipy.sparse import linalg as splinalg
-    import time, sys, os
-    from parallel import IF3DParallel, MASTER
-    from parutil import SaveLogCallback
-
-    ROOT = "/home/gomezs/IsoTurbData/"
-    #ROOT = "/master/home/gomezs/isoturb/"
-    _main()
