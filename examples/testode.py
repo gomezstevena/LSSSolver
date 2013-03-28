Removed extra example, fixed off-by-one error in computing trajectory length

examples/testode.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     dt = 0.02   # Time step size
     N = 2**11   # Number of time steps
-    tol = 1e-7  # Relative Tolerance for solver
+    tol = 1e-6  # Relative Tolerance for solver
 
 
     if LSS.parallel.MASTER: # Only output if Master node (i.e. Node 0)
@@ -16,8 +16,8 @@ if __name__ == '__main__':
 
 
     # Create Initial Trajectory or load from file
-    T_final = N*dt
-    T_windup = N*dt/10.0
+    T_final = (N+1)*dt
+    T_windup = (N+1)*dt/10.0
     traj = LSS.Trajectory( lorenz, time=(T_windup, T_final), dt=dt, filename='lorenz_data')
 
 
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     lsq = LSS.ShadowODE( traj, iters=(30,60), tol=tol )
     rhs = lsq.mapTraj( lorenz.dfdrho )
 
-    #Optional: callback logs iterations and outputs to stdout every skip iterations
+    # Optional: callback logs iterations and outputs to stdout every skip iterations
     call = LSS.callbacks.LogCallback(lsq, rhs, fname='ode_log.npy', skip=5 ) 
 
     # Solve System with rhs, uses Conjugate Gradient w/ Multigrid Preconditioner
@@ -56,9 +56,11 @@ if __name__ == '__main__':
         # Plot Cumulative sensivity
         plt.subplot(1,2,1)
 
-        plt.plot( traj.times, np.cumsum(vz)/np.r_[1:N+2] )
+        plt.plot( traj.times, vz)#np.cumsum(vz)/np.r_[1:N+2] )
         plt.xlabel(r'$t$')
-        plt.ylabel(r'Cumulative Mean Z Sensivity')
+        plt.ylabel(r'Instantaneous Mean Z Sensivity')
+        plt.plot([0, (N+1)*dt], [ddrho[2], ddrho[2]], '--r' )
+        plt.xlim( 0, (N+1)*dt )
 
         # Plot Residual over time
         plt.subplot(1,2,2)

trajectory.py
@@ -25,17 +25,19 @@ class BaseTrajectory (object):
             raise TypeError("Require either numeric or 2-tuple of numeric types for time paramater")
 
 
-        self.N = int( math.ceil( self.end_time / self.dt ) )
-        self.end_time = self.N * self.dt
+        self.N = int( math.ceil( self.end_time / self.dt ) ) - 1
+        self.end_time = (self.N+1)*self.dt
+
+        self.times = self.t0 + np.arange( 0, self.end_time, self.dt )
 
 
 
     def create(self):
         "Create the trajectory using Implicit-Trapezoidal scheme for integration"
         if self.windup:
-            Nw = int( math.ceil( self.windup_time/self.dt ) )
+            Nw = int( math.ceil( self.windup_time/self.dt ) ) - 1
             u0 = np.ones(self.system.dim) if self.u0 is None else self.u0
-            u = trapIntegrate(self.system, u0, self.dt, Nw, tol=self.tol, t0 = self.t0-Nw*self.dt)
+            u = trapIntegrate(self.system, u0, self.dt, Nw, tol=self.tol, t0 = self.t0-(Nw+1)*self.dt)
             u0 = u[-1]
 
         self.u  = trapIntegrate(self.system, u0, self.dt, self.N, tol=self.tol, t0=self.t0)
@@ -49,10 +51,6 @@ class BaseTrajectory (object):
     @property 
     def dim(self):
         return self.system.dim
-
-    @property 
-    def times(self):
-        return np.r_[:self.N+1]*self.dt + self.t0
 
     def __getitem__(self, args):
         return self.u[args]
@@ -75,11 +73,13 @@ class Trajectory (BaseTrajectory):
             u = data['u']
             self.N = len(u)-1
             self.u = u
-            self.end_time = self.N * self.dt
+            self.end_time = (self.N+1) * self.dt
             self.t0 = data['t0']
             self.tol = kwargs.pop('tol', 1e-8)
             self.u0 = self.u[0]
             self.system = data['system']
+            self.times = self.t0 + np.arange( 0, self.end_time, self.dt )
+
         else:
             super(Trajectory, self).__init__(*args, **kwargs)
             self.create()